30a80fe1b8d89c37e438aa23c497b8bffce0be00
Épicier <louis.sarthou@orange.fr>
2021-06-15 12:14:34 +0200

    Improve Breadth First Search algorithm

--- Solver.py
+++ Solver.py
@@ -17,32 +17,30 @@ def Astar_search(root, h):
                 queue.appendleft(child)
 
 
-def breadth_first_search(root, max_depth):
+def breadth_first_search(root, max_depth=30, verbose=True):
 
     if not root.puzzle.is_solvable:
-        print(f"Position \n{root.puzzle}\n is not solvable")
-        return
-    solution_found = False
+        if verbose : print(f"Position\n{root.puzzle}\nis not solvable")
+        return None, None
+    found = False
     solutions = []
-    last_layer_nodes = [root]
-    node_counter = []
-    for depth in range(max_depth):
-        this_layer_nodes = []
-        this_layer_node_counter = 0
-        for node in last_layer_nodes:
-            children = node.expand()
-            for child in children:
-                if child.is_goal_state:
-                    solutions.append(child)
-                    if not solution_found:
-                        print(f"{depth+1}-move solution(s) found !")
-                        solution_found = True
-                this_layer_nodes.append(child)
-                this_layer_node_counter += 1
-        last_layer_nodes = this_layer_nodes
-        node_counter.append(this_layer_node_counter)
-        if solution_found:
-            return solutions, depth+1
-        print(f"Depth {depth+1:2} completed : {this_layer_node_counter:8} nodes")
-    print(f"No solution found up to depth {max_depth}")
+    queue = collections.deque([root])
+    node = root
+    depth = 0
+    while node.depth <= max_depth:
+        if node.depth > depth:
+            depth = node.depth
+            if verbose : print(f"Searching at depth {depth:>5}")
+        node = queue.popleft()
+
+        if node.is_goal_state:
+            solutions.append(node)
+            if not found:
+                if verbose : print(f"{node.depth}-move solution(s) found !")
+                found, max_depth = True, node.depth
+        for child in node.expand() : queue.append(child)
+    if found:
+        return solutions, depth
+        if verbose : print(f"Depth {depth:2} completed")
+    if verbose : print(f"BFS: No solution found up to depth {max_depth} for position\n{root.puzzle}")
     return None, None
