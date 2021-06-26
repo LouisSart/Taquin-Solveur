b2c12e11f589a690c17b6dc494f2580768dea787
Épicier <louis.sarthou@orange.fr>
2021-06-26 12:57:12 +0200

    Run every solver on easy and hard positions

--- tests.py
+++ tests.py
@@ -3,21 +3,33 @@ from Node import Node
 from heuristics import *
 from Solver import *
 
-def run_solvers():
+easy = Puzzle((3,3), [[4, 3, 2], [1, 7, 5], [6, 8, 0]]) # Has two optimal solutions
+hard = Puzzle((3,3), [[4, 7, 6], [5, 1, 8], [0, 3, 2]])
 
-    easy = [[4, 3, 2], [1, 7, 5], [6, 8, 0]] # Has two optimal solutions
-    puzzle = Puzzle((3,3), easy)
-    sols = Astar_search(Node(puzzle), heuristic=manhattan)
-    for sol in sols:
-        print([node.puzzle.bt_pos for node in sol.path], sol.depth)
-    print ("*"*30)
-    sols = breadth_first_search(Node(puzzle))
-    for sol in sols:
-        print([node.puzzle.bt_pos for node in sol.path], sol.depth)
-    print ("*"*30)
-    sols = depth_first_search(Node(puzzle), max_depth=15)
-    for sol in sols:
-        print([node.puzzle.bt_pos for node in sol.path], sol.depth)
+def run_solvers(puzzle, *solvers):
+
+    def bt_index(pos):
+        i,j = pos
+        return 3*i+j
+    for solver in solvers:
+        print ("*"*30)
+        sols = solver(Node(puzzle))
+        for sol in sols:
+            print([bt_index(node.puzzle.bt_pos) for node in sol.path], sol.depth)
 
 if __name__ == "__main__":
-    run_solvers()
+    print("Running solvers for easy position :")
+    print(easy)
+    run_solvers(
+        easy,
+        breadth_first_search,
+        Astar_search,
+        depth_first_search,
+    )
+    print("\nRunning solvers for hard position :")
+    print(hard)
+    run_solvers(
+        hard,
+        Astar_search,
+        depth_first_search,
+    )
