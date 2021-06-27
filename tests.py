25b2c2a5305c9556122f65b7323e0f12c55e4ef8
Épicier <louis.sarthou@orange.fr>
2021-06-27 18:05:44 +0200

    A timing utility

--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 from Puzzle import Puzzle
 from Node import Node
 from heuristics import *
+import time
 from Solver import *
 
 easy = Puzzle((3,3), [[4, 3, 2], [1, 7, 5], [6, 8, 0]]) # Has two optimal solutions in 8 moves
@@ -8,6 +9,21 @@ hard = Puzzle((3,3), [[4, 7, 6], [5, 1, 8], [0, 3, 2]]) # Has nine optimal solut
 hard44 = Puzzle((4,4), [[2, 4, 7, 8], [10, 13, 6, 3], [5, 0, 1, 14], [12, 11, 9, 15]]) # Has five optimal solutions in 45 moves
 easy44 = Puzzle((4,4), [[14, 1, 9, 6], [4, 8, 12, 5], [7, 2, 3, 0], [10, 11, 13, 15]]) # Has one optimal solution in 45 moves
 
+def time_solvers(puzzles, *solvers):
+
+    def time_this_puzzle(puzzle, solvers):
+        times = []
+        for solver in solvers:
+            tic = time.process_time()
+            solver.solve(puzzle)
+            tac = time.process_time()
+            times.append(tac-tic)
+        return tuple(times)
+
+    times = []
+    for puzzle in puzzles:
+        times.append(time_this_puzzle(puzzle, solvers))
+    return times
 
 def run_solvers(puzzle, *solvers):
 
