bc4451af6f0d3f8016af6b66e7784494d44cdc3c
Épicier <louis.sarthou@orange.fr>
2021-07-02 22:27:54 +0200

    An outer line heuristic now ready to be used in the solvers

--- heuristics.py
+++ heuristics.py
@@ -1,3 +1,4 @@
+import pickle
 
 def manhattan(puzzle):
 
@@ -10,6 +11,23 @@ def manhattan(puzzle):
             I, J = tile//n, tile%n
             if tile: cumdist += abs(i-I) + abs(j-J)
     return cumdist
+
+class OuterLineHeuristic():
+
+    def __init__(self):
+        with open("outer_line_table.pkl", "rb") as f:
+            self.outer_line_dict = pickle.load(f)
+        self.idx = {0:0,2:1,5:2,6:3,7:4,8:5}
+        self.pos = [0,0,0,0,0,0]
+
+
+    def __call__(self, puzzle):
+        for i in range(3):
+            for j in range(3):
+                tile = puzzle.tiles[i][j]
+                if tile in (0,2,5,6,7,8):
+                    self.pos[self.idx[tile]] = (i,j)
+        return self.outer_line_dict[tuple(self.pos).__hash__()]
 
 def outer_line_manhattan(puzzle):
     """
