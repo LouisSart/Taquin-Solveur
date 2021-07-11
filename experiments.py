b71a46abce58308387c76fbffc08b2153d8ee00e
Épicier <louis.sarthou@orange.fr>
2021-07-11 17:19:59 +0200

    A 2x2 CP vs CO benchmark

--- experiments.py
+++ experiments.py
@@ -1,6 +1,43 @@
 import pickle
 from EpicSolver.heuristics import manhattan
 from EpicSolver.taquin import Taquin
+from EpicSolver.cube2 import Cube2
+
+def CO22_vs_CP22_benchmark():
+    with open("tables/22CO_table.pkl", "rb") as f:
+        co_table = pickle.load(f)
+
+    with open("tables/22CP_table.pkl", "rb") as f:
+        cp_table = pickle.load(f)
+
+    puzzle = Cube2()
+    N = 10000
+    values = []
+
+    for k in range(N):
+
+        puzzle.shuffle(23)
+        cph = cp_table[tuple(puzzle.CP).__hash__()]
+        coh = co_table[tuple(puzzle.CO).__hash__()]
+        values.append((cph, coh))
+
+    cp_wins = sum([int(cph>coh) for cph, coh in values])
+    co_wins = sum([int(coh>cph) for cph, coh in values])
+    cp_mean = sum([cph for cph, coh in values])/len(values)
+    co_mean = sum([coh for cph, coh in values])/len(values)
+    max_mean = sum(max(cph, coh) for cph, coh in values)/len(values)
+    cp_dom  = sum([cph-coh if cph>coh else 0 for cph, coh in values])/cp_wins
+    co_dom  = sum([coh-cph if coh>cph else 0 for cph, coh in values])/co_wins
+
+    print(f"Ran {N} heuristic computations")
+    print("CP wins:", cp_wins, "CO wins:", co_wins, f"Ties : {N-co_wins-cp_wins}")
+    print("CP mean :", cp_mean)
+    print("CO mean :", co_mean)
+    print("Max mean :", max_mean)
+    print("CP mean gain in wins", cp_dom)
+    print("CO mean gain in wins", co_dom)
+    print("CP score (total gain in wins/number of runs):", cp_dom*cp_wins/N)
+    print("CO score (total gain in wins/number of runs):", co_dom*co_wins/N)
 
 def manhattan_vs_outer_line_benchmark():
     with open("tables/outer_line_table.pkl", "rb") as f:
