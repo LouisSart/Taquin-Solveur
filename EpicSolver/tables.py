b8b20c5f901a822bcf461709c95d80ac8cb728ea
Épicier <louis.sarthou@orange.fr>
2021-08-06 15:05:09 +0200

    walking distance heuristic
    it is not that much better than the manhattan heuristic

--- EpicSolver/tables.py
+++ EpicSolver/tables.py
@@ -1,7 +1,102 @@
-import numpy as np, pickle, itertools as it
+import numpy as np, pickle, itertools as it, copy, collections
 from .taquin import Taquin
 from .solver import IDAstar
 from .cube2 import Cube2
+from .node import Node
+
+def build_walking_distance_table():
+    """
+        a breadth-first generator for the walking distance table
+    """
+
+    class VerticalWalkingTaquin:
+
+        """
+            Pseudo taquin puzzle class for generating vertical walking distance heuristic
+            Solved position :
+
+            self.board =
+                  from 0  from 1  from 2  from 3
+            row 0 [[3       0       0       0]
+            row 1  [0       4       0       0]
+            row 2  [0       0       4       0]
+            row 3  [0       0       0       4]]
+
+            self.bt_pos = 0
+        """
+
+        def __init__(self, size=4, board=None):
+            self.size = size
+            self.board = board or [[0]*i + [size] + [0]*(size-i-1) for i in range(size)]
+            self.board[0][0] -= 1
+            self.bt_pos = 0
+
+
+        def allowed_moves(self, previous=None):
+            forbidden = previous.forbidden_next if previous else None
+            return tuple(move for move in all_moves if move != forbidden and self.size>(self.bt_pos + move.step)>=0 and (self.board[self.bt_pos+move.step][move.col]))
+
+        def apply(self, move):
+            self.board[self.bt_pos+move.step][move.col] -=1
+            self.board[self.bt_pos][move.col] += 1
+            self.bt_pos += move.step
+
+        def copy(self):
+            return copy.deepcopy(self)
+
+        def coord(self):
+            return tuple(k for line in self.board for k in line) + (self.bt_pos,)
+
+    class VerticalMove:
+        """
+            step = 1 for a down move, -1 for an up move
+            col : the column to swap the blank tile with
+            forbidden_next : a reference to the opposite VerticalMove object
+        """
+        def __init__(self, step, col):
+            self.step = step
+            self.col = col
+            self.forbidden_next = None
+        def __str__(self):
+            return f"{self.step, self.col}"
+
+    ups = [VerticalMove(-1, i) for i in range(4)]
+    downs = [VerticalMove(1, i) for i in range(4)]
+    # a swap with the same column in the opposite direction
+    # comes back to the original position so we forbid
+    # this by linking opposite moves
+    for up, down in zip(ups, downs):
+        up.forbidden_next = down
+        down.forbidden_next = up
+
+    all_moves = tuple(ups + downs)
+
+    # Starting from the solved position we generate new positions using breadth-first
+    # Positions are stored in a dict with keys the hash of the tuple containing
+    # the flattened puzzle board followed by the blank tile position
+    # and values the depth at which the position was found
+    # example for the solved state :
+    # {(3,0,0,0,0,4,0,0,0,0,4,0,0,0,0,4,0).__hash__():0}
+    # Horizontal value for a given taquin puzzle can be found in the vertical table
+    # by reading the puzzle in columns and counting :
+    # (4,8,12)    as from 1st
+    # (1,5,9,13)  as from 2nd
+    # (2,6,10,14) as from 3rd
+    # (3,7,11,15) as from 4th
+    puzzle = VerticalWalkingTaquin()
+    root = Node(puzzle)
+    queue = collections.deque([root])
+    generated = {} # dict linking state hash to depth
+    while queue:
+        node = queue.popleft()
+        state = node.puzzle.coord()
+        if state.__hash__() not in generated:
+            generated.update({state.__hash__():node.depth}) # store newly encountered positions
+            print(len(generated), node.depth)
+            for child in node.expand(lambda puzzle: 0) : queue.append(child) # and generate their children
+
+    with open("vertical_wd_table.pkl", "wb") as f:
+        pickle.dump(generated, f)
 
 def build_22CP_table():
     """
