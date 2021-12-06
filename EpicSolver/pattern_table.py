Memory overflow fixed for big tables
8-tile pattern should take about 2.5 days XD

EpicSolver/pattern_table.py
@@ -97,6 +97,10 @@ class PatternTaquin:
         pstr = str(mt[(self.size, self.ntiles)][1][self.pindex]['permutation'])
         return "\n".join((lstr, pstr))
 
+def combine(current, new):
+    current = np.unpackbits(current)
+    return np.packbits([1 if a or b else 0 for a, b in zip(current, new)])
+
 def build_pattern_table(size, tiles, prefix=None):
 
     mt.empty()
@@ -106,40 +110,43 @@ def build_pattern_table(size, tiles, prefix=None):
     pattern = Pattern(size, tiles)
     puzzle = PatternTaquin()
     puzzle.from_taquin(taquin, pattern)
-    queue = HardQueue([Node(puzzle)])
+    queue = HardQueue((10**3, 10**6), np.dtype([('lindex', np.uint16), ('pindex', np.uint16), ('depth', np.uint8), ('reached_blanks', np.uint8, (2,))]))
     table = np.zeros((pattern.nlayt, pattern.nperm), dtype=np.uint8) # ex for 8-tile pattern: 40320*12870 = 500 Mo
     checked_bt_states = np.zeros((pattern.nlayt, pattern.nperm, 2), dtype=np.uint8) # ex for 8-tile pattern : 2*12870*40320 = 1,0 Go
-    checked_bt_states[puzzle.lindex, puzzle.pindex] = np.packbits(puzzle.reachable_bt_pos()) # combine the bt_pos arrays
-    counter, d = 0, 0
+    bts = np.packbits(puzzle.reachable_bt_pos())
+    checked_bt_states[puzzle.lindex, puzzle.pindex] = combine(checked_bt_states[puzzle.lindex, puzzle.pindex], bts) # combine the bt_pos arrays
+    queue.store((puzzle.lindex, puzzle.pindex, 0, bts))
+    counter, d = 1, 0
+    useless_counter = 0
     print(f"Generating table {pattern}")
     print(" d,     %, nodes")
-    print("-"*15)
+    print("-"*16)
 
     while queue.is_not_empty():
-        node = queue.pop() # Pop next node
+        lindex, pindex, depth, bts = queue.pop() # Pop next node
+        p = PatternTaquin()
+        p.lindex, p.pindex, p.reached = lindex, pindex, np.unpackbits(bts)
+        p.size, p.ntiles = size, len(pattern)
+        node = Node(p)
+        node.depth = depth
         if node.depth>d:
             print(f"{node.depth:2}", f"{counter/pattern.table_size*100:5.2f}%", counter)
             d = node.depth
         for child in node.expand():
             lindex, pindex = child.puzzle.lindex, child.puzzle.pindex
-            this_bts = checked_bt_states[lindex, pindex]
-            if table[lindex, pindex] == 0: # first time we see this pattern position
-                table[lindex, pindex] = node.depth # we store the depth
-                adj = child.puzzle.reachable_bt_pos()
-                queue.store(child) # store the child
-                chk_bts = np.unpackbits(this_bts)
-                checked_bt_states[lindex, pindex] = np.packbits([1 if a or b else 0 for a, b in zip(adj, chk_bts)]) # combine the bt_pos arrays
+            if table[lindex, pindex] == 0 and not (lindex, pindex) == (puzzle.lindex, puzzle.pindex): # first time we see this pattern position
+                table[lindex, pindex] = child.depth # we store the depth
+                reached = np.packbits(child.puzzle.reachable_bt_pos())
+                checked_bt_states[lindex, pindex] = reached # check reached bt pos
+                queue.store((child.puzzle.lindex, child.puzzle.pindex, child.depth, reached)) # store the child
                 counter += 1
-            elif np.unpackbits(this_bts)[child.puzzle.bt_pos] == 0: # if the pattern position has been seen but not with this bt_pos
-                adj = child.puzzle.reachable_bt_pos()
-                queue.store(child) # store the children
-                chk_bts = np.unpackbits(this_bts)
-                checked_bt_states[lindex, pindex] = np.packbits([1 if a or b else 0 for a, b in zip(adj, chk_bts)]) # combine the bt_pos arrays
-    print(f"{(counter-1)/pattern.table_size*100:5.2f}%", counter-1)
-    # When we do this we visit the solved position again at depth 2
-    # Since its stored h value is 0 at this point, it gets set to 2,
-    # so we need to reset it manually after the loop ends
-    table[puzzle.lindex, puzzle.pindex] = 0
+            elif np.unpackbits(checked_bt_states[lindex, pindex])[child.puzzle.bt_pos] == 0: # if the pattern position has been seen but not with this bt_pos
+                reached = child.puzzle.reachable_bt_pos()
+                checked_bt_states[lindex, pindex] = combine(checked_bt_states[lindex, pindex], reached) # combine the bt_pos arrays
+                queue.store((lindex, pindex, child.depth, np.packbits(reached))) # store the child
+            else:
+                useless_counter += 1
+                # print(useless_counter)
 
     print("Table generated")
 