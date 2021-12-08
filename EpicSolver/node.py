99e2cb2c031ae299422e75f93028cffa000ea4ad
Épicier <louis.sarthou@orange.fr>
2021-12-08 21:30:27 +0100

    Nodes now use the state implementation

--- EpicSolver/node.py
+++ EpicSolver/node.py
@@ -1,31 +1,24 @@
-from .heuristics import NoneHeuristic
-
-class Node:
-    def __init__(self, puzzle, heuristic=NoneHeuristic()):
-        self.puzzle = puzzle
+class StateNode:
+    def __init__(self, state):
+        self.state = state
         self.parent = None
-        self.heuristic = heuristic
-        self.heuristic.compute(self.puzzle)
         self.depth = 0
         self.move = None
+        self.h = state.estimate
 
     def expand(self):
 
         previous = self.move
         successors = []
-        for move in self.puzzle.allowed_moves(previous):
-            child = Child(self, move)
+        for move in self.state.valid_moves(previous):
+            child = StateChild(self, move)
             successors.append(child)
         return sorted(successors, key=lambda node: node.depth + node.h)
 
     @property
-    def h(self):
-        return self.heuristic.estimate
-
-    @property
     def is_goal_state(self):
-        if self.heuristic.estimate == 0:
-            return self.puzzle.is_solved
+        if self.state.estimate == 0:
+            return self.state.is_solved
         else: return False
 
     @property
@@ -38,12 +31,11 @@ class Node:
             node = node.parent
         return tuple(reversed(path))
 
-class Child(Node):
+class StateChild(Node):
     def __init__(self, parent, move):
         self.parent = parent
         self.move = move
-        self.puzzle = parent.puzzle.copy()
-        self.puzzle.apply(move)
         self.depth = parent.depth+1
-        self.heuristic = parent.heuristic.copy()
-        self.heuristic.update(self, move)
+        self.state = parent.state.copy()
+        self.state.update(move)
+        self.h = self.state.estimate
