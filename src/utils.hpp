6f1ec290f470d3e36bd777460cd6b172be07e36e
Louis Sarthou <louissarthou@gmail.com>
2024-09-16 22:00:01 +0200

    print_vector utility

--- src/utils.hpp
+++ src/utils.hpp
@@ -11,6 +11,17 @@ void print_array(const typename std::array<T, n> &a) {
   std::cout << "}" << std::endl;
 }
 
+template <typename T, typename cast_t = T>
+void print_vector(const typename std::vector<T> &v) {
+  std::cout << "{";
+  for (auto k : v) {
+    std::cout << static_cast<cast_t>(k) << ", ";
+  }
+  if (v.size() > 0)
+    std::cout << "\b\b";
+  std::cout << "}" << std::endl;
+}
+
 template <typename T, typename... Ts>
 void print(const T &truc, const Ts &...reste) {
   if constexpr (sizeof...(Ts) == 0) {
